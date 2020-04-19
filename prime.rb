52f24e2e01b82f6322734e07206ec3f98649b226
Mario Garza <mario.garza02@utrgv.edu>
2020-04-19 06:38:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,14 @@
 # Add  code here!
-def prime?(int)
-  if(int<2)
-    return false
-  end
+#def prime?(int)
+#  if(int<2)
+#    return false
+#  end
 
-  (2...int).each do |n|
-    if(int%n==0)
-      return false
-    end
-    end
-    true
-end
+#  (2...int).each do |n|
+#    if(int%n==0)
+#      return false
+#  end
+#   end
+#    true
+#end
+
